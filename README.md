b3aa0caed46c30f420b7e5b3d1e55a4a97739cf8
Milos Bejda <mbejda@live.com>
2019-08-18 02:50:55 -0400

    Hello Starter

---
+++ README.md
@@ -0,0 +1,4 @@
+# Lean Angularfire Starter 
+Angular 7+ Starter to get you up and running fast. 
+
+This starter is loaded with Angular 7
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: 'AIzaSyB_jcEP_X5I1SzZpZ-49G9-onpQQ9m-svQ',
+    authDomain: 'leanangularfirestarter.firebaseapp.com',
+    databaseURL: 'https://leanangularfirestarter.firebaseio.com',
+    projectId: 'leanangularfirestarter',
+    storageBucket: '',
+    messagingSenderId: '27804420334',
+    appId: '1:27804420334:web:cf155df81b572954'
+  }
+};
